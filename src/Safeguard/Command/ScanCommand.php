c1ddc007bd053fd2f695715690f9c4658488aa86
Iain Cambridge <iain.cambridge@kisura.de>
2016-04-30 15:22:08 +0200

    Create very basic scan command

---
+++ src/Safeguard/Command/ScanCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Safeguard\Command;
+
+use Safeguard\Collector\CodeFinder;
+use Safeguard\Parsers\AliasParser;
+use Safeguard\Parsers\ClassMethodParser;
+use Safeguard\Parsers\ClassParser;
+use Safeguard\Parsers\CodeParser;
+use Safeguard\Parsers\FileParser;
+use Safeguard\Parsers\FunctionParser;
+use Safeguard\Parsers\NamespaceParser;
+use Safeguard\Parsers\ParamParser;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ScanCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName("scan")
+            ->setDescription('Scans directory for invalid code')
+            ->addArgument('directory', InputArgument::REQUIRED, 'The directory to be scanned');
+    }
+
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name = $input->getArgument('directory');
+
+        $codeFinder = new CodeFinder();
+        $codeParser = new CodeParser();
+        $paramParser = new ParamParser();
+        $classMethodParser = new ClassMethodParser($paramParser);
+        $classParser = new ClassParser($classMethodParser);
+        $namespaceParser = new NamespaceParser($classParser);
+        $functionParser = new FunctionParser($paramParser);
+        $aliasParser = new AliasParser();
+        $fileParser = new FileParser($functionParser, $namespaceParser, $classParser, $aliasParser);
+
+        $realpath = realpath($name);
+        $files = $codeFinder->getFileNames($realpath);
+        foreach ($files as $filename) {
+            $nodes = $codeParser->parseCode($filename);
+            $file = $fileParser->processFile($nodes);
+        }
+
+        $message = sprintf("Finished %d files scanned", count($files));
+        $output->writeln($message);
+    }
+}
--- src/Safeguard/Parsers/ParamParser.php
+++ src/Safeguard/Parsers/ParamParser.php
@@ -29,7 +29,10 @@ class ParamParser
     protected function convertParam(ParamNode $param)
     {
         $name = $param->name;
-        $typeHint = implode("\\",  $param->type->parts);
+        $typeHint = null;
+        if (is_object($param->type) && $param->type->parts) {
+            $typeHint = implode("\\",  $param->type->parts);
+        }
 
         return new Param($name, $typeHint);
     }
